e5993c9a0d1b75067a95dcf5466b89a962b30855
SKM GEEK <qestudios17@gmail.com>
2025-06-11 15:15:44 +1000

    Initial commit with basic test ping command

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CMMRewrite
-A rewrite of the Circuit Maker Maker discord bot in Go.
+### A rewrite of the Circuit Maker Maker discord bot in Go.
+
+I found that the Circuit Maker Maker Discord bot was quite slow and I decided to do a full rewrite in Go.
---
+++ cmd/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/QEStudios/CMMRewrite/commands"
+	"github.com/bwmarrin/discordgo"
+	"github.com/lpernett/godotenv"
+)
+
+var discord *discordgo.Session
+var App string
+var Guild string
+
+func init() {
+	var err error
+	envFile, _ := godotenv.Read(".env")
+	var Token = envFile["BOT_TOKEN"]
+	App = envFile["APP_ID"]
+	Guild = envFile["GUILD_ID"]
+
+	discord, err = discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatalf("Could not login: %s", err)
+	}
+}
+
+var (
+	commandDefs = []*discordgo.ApplicationCommand{
+		&commands.PingDef,
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"ping": commands.HandlePing,
+	}
+)
+
+func main() {
+	discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as %s", r.User.String())
+		s.UpdateGameStatus(0, "Circuit Maker 2")
+	})
+
+	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if i.Type != discordgo.InteractionApplicationCommand {
+			return
+		}
+
+		data := i.ApplicationCommandData()
+		commandHandlers[data.Name](s, i)
+	})
+
+	_, err := discord.ApplicationCommandBulkOverwrite(App, Guild, commandDefs)
+	if err != nil {
+		log.Fatalf("Could not register commands: %s", err)
+	}
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatalf("Could not open session: %s", err)
+	}
+
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt)
+	<-sigch
+
+	err = discord.Close()
+	if err != nil {
+		log.Printf("Could not gracefully close session: %s", err)
+	}
+}
